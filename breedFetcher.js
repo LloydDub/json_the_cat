7f15f400c1429642437618989eba2dce77604476
Lloyd Worth <lloydrww@gmail.com>
2021-08-23 20:48:27 +0000

    refactored and created callback to handle edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,21 +3,24 @@ const request = require('request');
 
 
 
-let catBreedAPI = `https://api.thecatapi.com/v1/breeds/search/?q=${process.argv[2]}`
 
 
+const fetchBreedDescription = function(breedName, callback) {
+  let catBreedAPI = `https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`
 
-
-
-
-
-request(catBreedAPI, (error, response, body) => {
-  if (error) {
-    console.log('error', error);
+  request(catBreedAPI, (error, response, body) => {
+  if (error) { // handles error from connection
+    callback(error, null)
+    
   } else if (JSON.parse(body)[0] === undefined){
     console.log(`The breed ${process.argv[2]} is not found`);
   } else {
-    console.log(JSON.parse(body)[0]['description']);
+    
+    callback(null, JSON.parse(body)[0]['description'] )
   }  
 });
 
+};
+
+
+module.exports = { fetchBreedDescription };
