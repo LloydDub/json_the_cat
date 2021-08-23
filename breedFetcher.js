8fe186b840af7f4e353563c31686639220fd8120
Lloyd Worth <lloydrww@gmail.com>
2021-08-23 21:13:19 +0000

    Mocha nad chai testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,19 +6,19 @@ const request = require('request');
 
 
 const fetchBreedDescription = function(breedName, callback) {
-  let catBreedAPI = `https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`
+  let catBreedAPI = `https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`;
 
   request(catBreedAPI, (error, response, body) => {
-  if (error) { // handles error from connection
-    callback(error, null)
+    if (error) { // handles error from connection
+      callback(error, null);
     
-  } else if (JSON.parse(body)[0] === undefined){
-    console.log(`The breed ${process.argv[2]} is not found`);
-  } else {
+    } else if (JSON.parse(body)[0] === undefined) {
+      console.log(`The breed ${process.argv[2]} is not found`);
+    } else {
     
-    callback(null, JSON.parse(body)[0]['description'] )
-  }  
-});
+      callback(null, JSON.parse(body)[0]['description']);
+    }
+  });
 
 };
 
