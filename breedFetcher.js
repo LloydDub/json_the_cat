5b7ef030b40454394a09c47bf39647302afe32d3
Lloyd Worth <lloydrww@gmail.com>
2021-08-23 19:42:26 +0000

    Added edge cases and re put in cat description part

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,17 +3,21 @@ const request = require('request');
 
 
 
-let catBreedAPI = `https://api.thecatapi.com/v1/breeds/search/?q=${process.argv[2]}`;
+let catBreedAPI = `https://api.thecatapi.com/v1/breeds/search/?q=${process.argv[2]}`
+
+
+
+
 
 
 
 request(catBreedAPI, (error, response, body) => {
   if (error) {
     console.log('error', error);
-  }
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
+  } else if (JSON.parse(body)[0] === undefined){
     console.log(`The breed ${process.argv[2]} is not found`);
-  }
+  } else {
+    console.log(JSON.parse(body)[0]['description']);
+  }  
 });
 
